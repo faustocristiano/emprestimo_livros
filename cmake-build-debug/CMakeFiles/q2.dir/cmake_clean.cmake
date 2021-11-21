d399d095c86ad80ae653c942beda8b8a1a5db063
Fausto Cristiano <fausto.c@aluno.ifsc.edu.br>
2021-11-21 18:40:57 -0300

    concluído

---
+++ cmake-build-debug/CMakeFiles/q2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/q2.dir/main.c.o"
+  "q2"
+  "q2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/q2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
